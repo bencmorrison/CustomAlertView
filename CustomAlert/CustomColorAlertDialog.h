24797d635c87c1b1ca307f554bdf4a564f74808f
Ben Morrison <bencmorrison@me.com>
2012-12-05 21:14:43 -0800

    Commiting

---
+++ CustomAlert/CustomColorAlertDialog.h
@@ -0,0 +1,13 @@
+//
+//  CustomColorAlertDialog.h
+//  CustomAlert
+//
+//  Created by Ben Morrison on 11/16/12.
+//  Copyright (c) 2012 Ben Morrison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomColorAlertDialog : UIViewController
+
+@end
